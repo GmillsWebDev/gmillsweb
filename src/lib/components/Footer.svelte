bcac231072d48f46a0c144402cf93a9e85eebed0
Gary Mills <gmillswebdev@gmail.com>
2025-09-07 17:46:58 +0100

    testing svg borders, footer started

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,40 @@
+<script>
+        let year = new Date().getFullYear();
+</script>
+
+<footer>
+    
+<div class="container-full">
+<svg class="borderwave" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1800" height="250" preserveAspectRatio="none" viewBox="0 0 1800 250">
+    <g mask="url(&quot;#SvgjsMask1075&quot;)" fill="none">
+        <rect width="1800" height="250" x="0" y="0" fill="rgba(0, 4, 32, 1)"></rect>
+        <path d="M 0,31 C 120,57.4 360,150.8 600,163 C 840,175.2 960,95.2 1200,92 C 1440,88.8 1680,136 1800,147L1800 250L0 250z" fill="rgba(33, 94, 88, 1)"></path>
+    </g>
+    <defs>
+        <mask id="SvgjsMask1075">
+            <rect width="1800" height="250" fill="#ffffff"></rect>
+        </mask>
+    </defs>
+</svg>
+<div class="container">
+    <p>© {year} - G Mills Web</p>
+    <div class="footer-links">
+        <a href="/privacy-policy">Privacy Policy</a>
+        <a href="/terms-and-conditions">Terms & Conditions</a>
+        <a href="/contact">Contact</a>
+    </div>
+</div>
+</div>
+</footer>
+
+<style>
+    footer{
+        background: var(--colour-secondary);
+        padding-bottom: 2rem;
+    }
+
+    .borderwave {
+        width: 100%;
+        height: 7rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,47 @@
 <h2>Some more text</h2>
 <p>text with a <a href="#">link</a></p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, consequatur! Ut et, exercitationem quas provident harum quos aliquid id molestiae facilis dolore libero quasi iusto unde quam illo iste quisquam?</p>
+
+
+</div>
+<div class="container-full light">
+    <svg class="borderwave" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1800" height="250" preserveAspectRatio="none" viewBox="0 0 1800 250">
+    <g mask="url(&quot;#SvgjsMask1019&quot;)" fill="none">
+        <rect width="100%" height="250" x="0" y="0" fill="rgba(0, 4, 32, 1)"></rect>
+        <path d="M 0,166 C 90,142.6 270,47.4 450,49 C 630,50.6 720,174.2 900,174 C 1080,173.8 1170,41.4 1350,48 C 1530,54.6 1710,175.2 1800,207L1800 250L0 250z" fill="rgba(229, 231, 235, 1)"></path>
+    </g>
+    <defs>
+        <mask id="SvgjsMask1019">
+            <rect width="100%" height="250" fill="#ffffff"></rect>
+        </mask>
+    </defs>
+</svg>
+<div class="container">
+    <p>Some text</p>
+    <h1>Testing</h1>
+    <p>Text with a <a href="#">link</a></p>
+</div>
+<svg class="borderwave" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1800" height="150" preserveAspectRatio="none" viewBox="0 0 1800 150">
+    <g mask="url(&quot;#SvgjsMask1096&quot;)" fill="none">
+        <rect width="1800" height="150" x="0" y="0" fill="rgba(229, 231, 235, 1)"></rect>
+        <path d="M 0,43 C 120,54 360,104.8 600,98 C 840,91.2 960,8 1200,9 C 1440,10 1680,84.2 1800,103L1800 150L0 150z" fill="rgba(0, 4, 32, 1)"></path>
+    </g>
+    <defs>
+        <mask id="SvgjsMask1096">
+            <rect width="1800" height="150" fill="#ffffff"></rect>
+        </mask>
+    </defs>
+</svg>
+</div>
+<div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, distinctio alias. Fuga aliquid nemo ut possimus numquam aperiam laudantium nobis tenetur rerum eaque, vero eveniet natus eos. Architecto, excepturi recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, distinctio alias. Fuga aliquid nemo ut possimus numquam aperiam laudantium nobis tenetur rerum eaque, vero eveniet natus eos. Architecto, excepturi recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, distinctio alias. Fuga aliquid nemo ut possimus numquam aperiam laudantium nobis tenetur rerum eaque, vero eveniet natus eos. Architecto, excepturi recusandae.</p>
 
 </div>
